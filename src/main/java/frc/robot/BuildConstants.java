fd28664cd7bb09a1ac5a9eaaacf7451427c1608a
Adam Reeves <33359031+Areeves977@users.noreply.github.com>
2023-02-03 12:13:40 -0500

    update ignore

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "977 2023 Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 6;
-  public static final String GIT_SHA = "2716a9b7bba5fbd3e4a357e7acf87863238d3002";
-  public static final String GIT_DATE = "2023-01-25 15:06:07 EST";
+  public static final int GIT_REVISION = 13;
+  public static final String GIT_SHA = "456b22a433cc97eedac81970d92da70b41a75366";
+  public static final String GIT_DATE = "2023-02-01 16:46:51 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2023-01-28 12:00:50 EST";
-  public static final long BUILD_UNIX_TIME = 1674925250942L;
+  public static final String BUILD_DATE = "2023-02-01 16:49:09 EST";
+  public static final long BUILD_UNIX_TIME = 1675288149048L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
